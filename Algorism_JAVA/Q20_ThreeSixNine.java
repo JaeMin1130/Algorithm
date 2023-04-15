7b66522fcc0b1d24afa90e3643b43a9be10e5344
JaeMin1130 <ija06598@gmail.com>
2023-04-15 12:08:37 +0900

    20. 369 게임

--- Algorism_JAVA/Q20_ThreeSixNine.java
+++ Algorism_JAVA/Q20_ThreeSixNine.java
@@ -1,24 +1,24 @@
 public class Q20_ThreeSixNine {
     private static int count(int i) {
         int times = 0;
-       do {
+        do {
             if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
                 times++;
-            } 
+            }
             i = i / 10;
-        } while(i > 1);
+        } while (i > 0);
         return times;
     }
 
     public static void main(String[] args) {
-          for (int i = 1; i <= 100; i++) {
-            if(count(i) == 0){
+        for (int i = 1; i <= 100; i++) {
+            if (count(i) == 0) {
                 System.out.print(i + " ");
-            }else if(count(i) == 1){
+            } else if (count(i) == 1) {
                 System.out.print("짝" + " ");
-            }else{
+            } else {
                 System.out.print("짝짝" + " ");
             }
-          }
         }
     }
+}
