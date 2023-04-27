212ba13934ffea4520be16d25f280a3d934813a2
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-04-27 15:45:07 +0900

    [Bronze I] Title: 팰린드롬수, Time: 204 ms, Memory: 17436 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,19 +1,15 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static void palin() {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<String> result = new ArrayList<String>();
-
         while (true) {
-            int input = scanner.nextInt();
-            if (input == 0)
+            String input = scanner.next();
+            if (input.equals("0"))
                 break;
 
-            String inputStr = Integer.toString(input);
-            char[] arr = (inputStr).toCharArray();
+            char[] arr = (input).toCharArray();
             int count = 0;
 
             for (int i = 0; i < arr.length / 2; i++) {
@@ -23,14 +19,12 @@ public class Main {
             }
 
             if (count == arr.length / 2) {
-                result.add("yes");
+                System.out.println("yes");
             } else {
-                result.add("no");
+                System.out.println("no");
             }
         }
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println(result.get(i));
-        }
+
     }
 
     public static void main(String[] args) {
