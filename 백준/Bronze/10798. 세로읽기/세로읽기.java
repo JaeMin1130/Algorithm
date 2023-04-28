7453cac21b08468cf6415f27ac36df14b3f79c71
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-04-28 21:50:07 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 14512 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = new String[5];
+        char[][] arr = new char[5][15];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = bf.readLine();
+            arr[i] = s[i].toCharArray();
+        }
+        for (int i = 0; i < 75; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                try {
+                    System.out.print(arr[j][i]);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.print("");
+                }
+            }
+        }
+    }
+}
