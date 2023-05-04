096b7d1a53d95d7894f3b1ed4dcfbfd179b8d03b
JaeMin1130 <ija06598@gmail.com>
2023-05-04 17:59:07 +0900

    문자열로 풀어야함

--- Main.java
+++ Main.java
@@ -1,27 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Convert {
+    private static int decimal;
+
+    static void toDecimal() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder biNum = new StringBuilder(br.readLine());
+        char[] revArr = biNum.reverse().toString().toCharArray();
+
+        for (int i = 0; i < revArr.length; i++) {
+            if (revArr[i] == '1') {
+                decimal += (int) (Math.pow(2, i));
+            }
+        }
+    }
+
+    static void toOctal() throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        while (decimal / 8 != 0) {
+            stack.push(decimal % 8);
+            decimal /= 8;
+        }
+        stack.push(decimal);
+        while (!stack.empty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
 
 public class Main {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        long num = scanner.nextLong();
-        scanner.close();
-
-        long cat = 1;
-        int count = 1;
-
-        while (num > cat) {
-            num -= cat;
-            cat *= 2;
-            count++;
-        }
-
-        if (num == 0) {
-            System.out.println(0);
-        } else if (num == cat) {
-            System.out.println(count);
-        } else {
-            System.out.println(count + 1);
-        }
+    public static void main(String[] args) throws IOException {
+        Convert.toDecimal();
+        Convert.toOctal();
     }
 }
