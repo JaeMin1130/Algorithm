a1425718586dc6990cb0142c70d2be28b59ad92f
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-05-04 23:40:16 +0900

    [Bronze I] Title: 2진수 8진수, Time: 580 ms, Memory: 98276 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+class Convert {
+
+    static void toOctal() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<String> stack = new Stack<>();
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(String.valueOf(input.charAt(i)));
+        }
+
+        int sum = 0;
+        int i = 0;
+        int[] arr = { 1, 2, 4 };
+        Stack<String> octal = new Stack<>();
+
+        while (!stack.empty()) {
+            while (i < arr.length) {
+                if (stack.pop().equals("1")) {
+                    sum += arr[i++];
+                } else
+                    i++;
+                if (stack.empty())
+                    break;
+
+            }
+            octal.push(String.valueOf(sum));
+            sum = 0;
+            i = 0;
+        }
+        while (!octal.empty()) {
+            bw.write(octal.pop());
+        }
+        bw.close();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Convert.toOctal();
+    }
+}
