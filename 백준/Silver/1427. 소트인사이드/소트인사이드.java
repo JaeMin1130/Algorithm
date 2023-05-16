f9956af15fed4654520b2c4866460b45289d46cc
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-05-16 09:59:30 +0900

    [Silver V] Title: 소트인사이드, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if ((int) arr[i] < (int) arr[j]) {
+                    char store = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = store;
+                }
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + "");
+        }
+        bw.close();
+    }
+}
