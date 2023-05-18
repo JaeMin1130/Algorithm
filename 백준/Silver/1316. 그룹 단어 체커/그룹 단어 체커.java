99b3215539f6b96eff7621a5645e25a5cbca175e
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-05-18 14:11:57 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -5,15 +5,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+    static void reset(boolean[] array){
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]) array[i] = false;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int num = Integer.parseInt(br.readLine());
         int count = 0;
+        boolean alphabet[] = new boolean[26];
+        boolean flag = true;
 
         for (int i = 0; i < num; i++) {
-            boolean alphabet[] = new boolean[26];
-            boolean flag = true;
             char[] word = br.readLine().toCharArray();
             for (int j = 1; j < word.length; j++) {
                 if (word[j - 1] != word[j]) {
@@ -24,8 +29,9 @@ public class Main {
                     break;
                 }
             }
-            if (flag)
-                count++;
+            if (flag) count++;
+            else flag = true; 
+            reset(alphabet);
         }
         bw.write(String.valueOf(count));
         bw.close();
