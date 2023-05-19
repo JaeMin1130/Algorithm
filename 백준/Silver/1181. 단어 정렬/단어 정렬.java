f3819dd0a09a464a2241c9440f91602ab9d4e27d
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-05-20 00:47:21 +0900

    [Silver V] Title: 단어 정렬, Time: 1572 ms, Memory: 26000 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,46 +4,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Main {
+class StringData implements Comparable<StringData> {
+    private String str;
+    private int length;
 
-    public static void main(String[] args) throws IOException {
+    StringData(String str, int length) {
+        this.str = str;
+        this.length = str.length();
+    }
+
+    public static StringData[] putData() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int num = Integer.parseInt(br.readLine());
-        String[] arr = new String[num];
-        String store;
-        ArrayList<String> result = new ArrayList<String>();
+        StringData[] arr = new StringData[num];
         for (int i = 0; i < num; i++) {
-            arr[i] = br.readLine();
+            String data = br.readLine();
+            arr[i] = new StringData(data, data.length());
         }
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i].length() > arr[j].length()) {
-                    store = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = store;
-                }
+        return arr;
+    }
+
+    @Override
+    public int compareTo(StringData data) {
+        int value = this.length - data.length;
+        if (value == 0) {
+            value = this.str.compareTo(data.str);
+        }
+        return value;
+    }
+
+    public static ArrayList<String> removeDup(StringData[] array) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(array[0].str);
+        for (int i = 1; i < array.length; i++) {
+            if (list.indexOf(array[i].str) == -1) {
+                list.add(array[i].str);
             }
         }
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i].length() == arr[j].length() && arr[i].compareTo(arr[j]) > 0) {
-                    store = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = store;
-                }
-            }
-        }
-        result.add(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            if (result.indexOf(arr[i]) == -1) {
-                result.add(arr[i]);
-            }
-        }
-        for (int i = 0; i < result.size(); i++) {
-            bw.write(result.get(i) + "\n");
+        return list;
+    }
+
+    public static void getStr(ArrayList<String> array) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < array.size(); i++) {
+            bw.write(array.get(i) + "\n");
         }
         bw.close();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringData[] arr = StringData.putData();
+        Arrays.sort(arr);
+        ArrayList<String> list = StringData.removeDup(arr);
+        StringData.getStr(list);
     }
 }
