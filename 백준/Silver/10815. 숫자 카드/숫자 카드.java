701cc0ee4c301295ba4cf65af668bc02cea26ba2
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-05-27 15:53:28 +0900

    [Silver V] Title: 숫자 카드, Time: 1352 ms, Memory: 120448 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num1 = Integer.parseInt(br.readLine());
+        String[] input1 = br.readLine().split(" ");
+        int[] ownCard = new int[num1];
+        for (int i = 0; i < input1.length; i++) {
+            ownCard[i] = Integer.parseInt(input1[i]);
+        }
+
+        int num2 = Integer.parseInt(br.readLine());
+        String[] input2 = br.readLine().split(" ");
+        int[] check = new int[num2];
+        for (int i = 0; i < check.length; i++) {
+            check[i] = Integer.parseInt(input2[i]);
+        }
+        Arrays.sort(ownCard);
+
+        for (int i = 0; i < check.length; i++) {
+            if (Arrays.binarySearch(ownCard, check[i]) >= 0) {
+                bw.write(1 + " ");
+            } else {
+                bw.write(0 + " ");
+            }
+        }
+        bw.close();
+    }
+}
