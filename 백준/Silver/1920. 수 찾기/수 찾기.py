bd5fb386f97185b9c71ad6d608e299f7b62fa18d
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-06-13 23:46:54 +0900

    [Silver IV] Title: 수 찾기, Time: 136 ms, Memory: 50412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,5 @@
-def binary_search(i, arr, start, end):
-    idx = (start + end) // 2
-    if(start < end):
-        if i > arr[idx]:
-            start = idx+1
-            return binary_search(i, arr, start, end)
-        elif i < arr[idx]:
-            end = idx        
-            return binary_search(i, arr, start, end)
-        else:
-            return 1
-    elif start == end:
-        if i == arr[idx]:
-            return 1
-        else:
-            return 0
-    else:
-        return 0
-
 N = int(input())
-arr = list(map(int, input().split()))
-arr = sorted(arr)
+arr = set(input().split())
 M = int(input())
-check_arr = list(map(int, input().split()))
-
-for i in check_arr:
-    print(binary_search(i, arr, 0, len(arr) - 1))
+result = map(lambda i: 1 if i in arr else 0, input().split())
+print(*result, sep="\n")
