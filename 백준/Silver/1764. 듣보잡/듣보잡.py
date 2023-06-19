600cd086e14c476a0812d2c58e4fca5ba5a6b12a
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-06-19 20:20:57 +0900

    [Silver IV] Title: 듣보잡, Time: 108 ms, Memory: 42072 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,14 @@
+from collections import Counter
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+Narr = set([input().rstrip() for _ in range(N)])
+
+name = [input().rstrip() for _ in range(M)]
+
+result = list(filter(lambda i: i in Narr, name))
+result = sorted(result)
+
+print(len(result))
+print(*result, sep="\n")
