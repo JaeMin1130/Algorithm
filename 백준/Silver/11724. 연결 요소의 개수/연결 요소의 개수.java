6ad65d4fa0a3b40e1897a35d0812a47dc7d63c2d
JaeMin1130 <ija06598@gmail.com>
2023-06-27 22:47:38 +0900

    06/27

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] nums = br.readLine().split(" ");
