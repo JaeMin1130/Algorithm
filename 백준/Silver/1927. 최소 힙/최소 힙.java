453024e37d4aebb5bf5acb4cc7bccbde193b75c7
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-07-01 21:56:34 +0900

    [Silver II] Title: 최소 힙, Time: 320 ms, Memory: 26904 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+class Main {
+    static PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int key = Integer.parseInt(br.readLine());
+            if (key == 0) {
+                if (heap.isEmpty()) {
+                    result.append(0).append("\n");
+                } else {
+                    result.append(heap.poll()).append("\n");
+                }
+            } else {
+                heap.offer(key);
+            }
+        }
+        System.out.println(result);
+    }
+}
