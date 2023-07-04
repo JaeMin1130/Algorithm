f210820227b777727a3f002c69bda72d0db0f587
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-07-04 10:53:04 +0900

    [Silver II] Title: 색종이 만들기, Time: 168 ms, Memory: 16332 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -9,36 +9,32 @@ class Main {
 
     static void drawLine(int startNum, int[] idx, int len) {
         boolean flag = false;
-        if (len == 1) {
-            count[startNum]++;
-            return;
-        } else {
-            for (int i = idx[0]; i < idx[0] + len; i++) {
-                for (int j = idx[1]; j < idx[1] + len; j++) {
-                    if (board[i][j] != startNum) {
+        for (int i = idx[0]; i < idx[0] + len; i++) {
+            for (int j = idx[1]; j < idx[1] + len; j++) {
+                if (board[i][j] != startNum) {
 
-                        int[] idx1 = { idx[0], idx[1] + len / 2 };
-                        int[] idx2 = { idx[0] + len / 2, idx[1] };
-                        int[] idx3 = { idx[0] + len / 2, idx[1] + len / 2 };
+                    int[] idx1 = { idx[0], idx[1] + len / 2 };
+                    int[] idx2 = { idx[0] + len / 2, idx[1] };
+                    int[] idx3 = { idx[0] + len / 2, idx[1] + len / 2 };
 
-                        drawLine(startNum, idx, len / 2);
-                        drawLine(board[idx1[0]][idx1[1]], idx1, len / 2);
-                        drawLine(board[idx2[0]][idx2[1]], idx2, len / 2);
-                        drawLine(board[idx3[0]][idx3[1]], idx3, len / 2);
+                    drawLine(startNum, idx, len / 2);
+                    drawLine(board[idx1[0]][idx1[1]], idx1, len / 2);
+                    drawLine(board[idx2[0]][idx2[1]], idx2, len / 2);
+                    drawLine(board[idx3[0]][idx3[1]], idx3, len / 2);
 
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag) {
+                    flag = true;
                     break;
                 }
             }
-            if (!flag) {
-                count[startNum]++;
+            if (flag) {
+                break;
             }
         }
+        if (!flag) {
+            count[startNum]++;
+        }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
