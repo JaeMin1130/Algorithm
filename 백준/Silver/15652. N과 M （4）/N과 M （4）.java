622986b6c3f96ed0c3010ef11b2324116fb43c45
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-08-29 22:31:37 +0900

    [Silver III] Title: N과 M (4), Time: 312 ms, Memory: 19248 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        getArr(0, 1);
+    }
+
+    private static void getArr(int idx, int num) {
+        if (idx == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int i : arr) {
+                sb.append(i + " ");
+            }
+            System.out.println(sb.toString().trim());
+            return;
+        }
+
+        for (int i = num; i <= N; i++) {
+            arr[idx] = i;
+            getArr(idx + 1, i);
+        }
+    }
+}
