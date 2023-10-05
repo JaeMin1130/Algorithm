9df95cad81c478d3b5b76ddba69fd56d787dbd94
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-10-05 23:25:25 +0900

    [Silver V] Title: K번째 수, Time: 2412 ms, Memory: 684732 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+
+        int[] arr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(arr);
+        System.out.println(arr[K - 1]);
+    }
+}
