70917057539b5699217debf92bc62903bf89803d
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-10-23 02:35:42 +0900

    [level 2] Title: 올바른 괄호, Time: 5.98 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,20 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int countBracket1 = 0;
-        int countBracket2 = 0;
+        int count = 0;
         char[] arr = s.toCharArray();
         for(char letter : arr){
             if(letter == '('){
-                countBracket1++;
+                count++;
             }else{
-                countBracket2++;
-                if(countBracket2 > countBracket1){
+                count--;
+                if(count < 0) {
                     answer = false;
                     break;
                 }
             }
         }
-        if(countBracket1 != countBracket2){
+        if(count != 0){
             answer = false;
         }
         return answer;
