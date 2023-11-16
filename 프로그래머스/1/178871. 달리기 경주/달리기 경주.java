4ac88b718b433259b5ce404ec90eaab6f31a2c4a
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-11-16 23:27:48 +0900

    [level 1] Title: 달리기 경주, Time: 0.07 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
+        
         String[] answer = new String[players.length];
         
         Map<String, Integer> map = new HashMap<>();
@@ -11,16 +12,15 @@ class Solution {
         
         for(String calling : callings){
             int idx = map.get(calling);
-            map.put(calling, idx - 1);
             String player = players[idx - 1];
+            
+            map.put(calling, idx - 1);
             map.put(player, idx);
+            
             players[idx] = player;
             players[idx - 1] = calling;
         }
         
-        for(String player : map.keySet()){
-            answer[map.get(player)] = player; 
-        }
-        return answer;
+        return players;
     }
 }
