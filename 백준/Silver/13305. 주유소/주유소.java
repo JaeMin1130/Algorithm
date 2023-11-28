4b16d5f870abda0da9e27c9090af9e826eb0894c
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2023-11-28 18:44:10 +0900

    [Silver III] Title: 주유소, Time: 392 ms, Memory: 34852 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class Main{
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] roads = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] cities = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        
+        int sum = 0;
+        for (int i = 0; i < cities.length - 1; i++) {
+            int count = checkPrice(i, cities);
+            int price = cities[i];
+            for (int j = i; j <= i + count; j++) {
+                sum += price * roads[j];
+            }
+            i += count;
+        }
+
+        System.out.println(sum);
+
+    }
+
+    static int checkPrice(int curLocation, int[] cities){
+
+        int count = 0;
+
+        for (int i = curLocation + 1; i < cities.length - 1; i++) {
+            if(cities[curLocation] <= cities[i]){
+                count++;
+            }else{
+                break;
+            }
+        }
+
+        return count;
+
+    }
+
+}
