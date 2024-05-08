bf27a68a76abf596f73b1a6d6ad1e4d8ce347fc4
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2024-05-08 13:11:34 +0900

    [Gold IV] Title: 알파벳, Time: 1196 ms, Memory: 294776 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int row;
     static int col;
     static char[][] board;
+    static boolean[] isAppeared = new boolean[26];
     static int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     static int count = 0;
 	public static void main(String[] args) throws IOException {
@@ -22,26 +21,23 @@ public class Main {
             board[i] = br.readLine().toCharArray();
         }
         
-        boolean[] isAppeared = new boolean[26];
-        Queue<Object[]> q = new LinkedList<>();
-        q.add(new Object[]{0, 0, 1, isAppeared});
         isAppeared[board[0][0] - 65] = true;
 
-        dfs(new int[]{0, 0}, 1, isAppeared);
+        dfs(new int[]{0, 0}, 1);
         System.out.println(count);
     }
 
-    static void dfs(int[] curLoc, int value, boolean[] isAppeared){
+    static void dfs(int[] curLoc, int value){
         for(int[] dir : directions){
             int nx = curLoc[0] + dir[0];
             int ny = curLoc[1] + dir[1];
             
             if(nx < 0 || nx >= row || ny < 0 || ny >= col) continue;
-            if(isAppeared[board[nx][ny] - 65]) continue;
+            if(isAppeared[board[nx][ny] - 'A']) continue;
 
-            isAppeared[board[nx][ny] - 65] = true;
-            dfs(new int[]{nx, ny}, value + 1, isAppeared.clone());
-            isAppeared[board[nx][ny] - 65] = false;
+            isAppeared[board[nx][ny] - 'A'] = true;
+            dfs(new int[]{nx, ny}, value + 1);
+            isAppeared[board[nx][ny] - 'A'] = false;
         }
 
         count = Math.max(count, value);
