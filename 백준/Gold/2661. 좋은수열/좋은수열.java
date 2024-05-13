899352094b3c8076c38289085784f6711dec4cff
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2024-05-13 17:46:06 +0900

    [Gold IV] Title: 좋은수열, Time: 156 ms, Memory: 19516 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.java
+++ 백준/Gold/2661. 좋은수열/좋은수열.java
@@ -4,39 +4,39 @@ import java.io.InputStreamReader;
 
 public class Main {
     static String answer = "iru";
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         StringBuilder st = new StringBuilder("");
+
         recur(st, n);
 
         System.out.println(answer);
     }
 
-    static void recur(StringBuilder st, int n){
-        if(st.length() == n){
+    static void recur(StringBuilder st, int n) {
+        if (st.length() == n) {
             answer = st.toString();
             return;
-        } 
+        }
 
         for (int i = 1; i <= 3; i++) {
-            if(!answer.equals("iru")) return;
-            
+            if (!answer.equals("iru")) return;
+
             st.append(i);
-            
+
             if (isGood(st)) recur(st, n);
-            
+
             st.deleteCharAt(st.length() - 1);
-            
-            if(!answer.equals("iru")) return;
         }
     }
 
-    static boolean isGood(StringBuilder answer) {
-        for (int i = 0; i < answer.length() - 1; i++) {
-            for (int j = i + 1; j < answer.length(); j++) {
-                if (answer.substring(i, j).equals(answer.substring(j))) return false;
+    static boolean isGood(StringBuilder st) {
+        for (int i = 1; i <= st.length() / 2; i++) {
+            for (int j = 0; j <= st.length() - (i * 2); j++) {
+                if (st.substring(j, j + i).equals(st.substring(j + i, j + i * 2))) return false;
             }
         }
         return true;
