0148ed95bab4b600d585f26beb9c896e552a373b
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2024-05-23 13:54:46 +0900

    [Silver I] Title: 곱셈, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static long C;
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		C = Long.parseLong(st.nextToken());
+		
+		System.out.println(pow(A, B));
+	}
+	
+	// A^exponent
+	public static long pow(long A, long exponent) {
+		
+		// 지수가 1일 경우 A^1 이므로 A를 그대로 리턴
+		if(exponent == 1) return A % C;
+		
+		// 지수의 절반에 해당하는 A^(exponent / 2) 을 구한다.
+		long temp = pow(A, exponent / 2);
+		
+		/*
+		 * 현재 지수가 홀수였다면 A를 한 번 더 곱해주어야 한다.
+		 * ex) A^9 = A^4 * A^4 * A
+         * (x * y) % z = (x % z) * (y % z) % z 에 따라,
+         * temp * temp * A % C = (temp * temp % C) * A % C
+		 */
+		if(exponent % 2 == 1) return (temp * temp % C) * A % C;
+		
+		return temp * temp % C;
+	}
+}
