f510406f80b89ce79b7b912b7e8f917118846748
JaeMin1130 <98063854+JaeMin1130@users.noreply.github.com>
2025-12-16 11:03:41 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 72 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -1,11 +1,15 @@
-n = int(input())
-answer = 0
-gomgom = set()
-for i in range(n):
-    str = input()
-    if(str == 'ENTER'):
-        answer += len(gomgom)
-        gomgom.clear()
-    else:
-        gomgom.add(str)
-print(answer + len(gomgom))
+import sys
+def problem_25192():
+    n = int(input())
+    answer = 0
+    gomgom = set()
+    for i in range(n):
+        str = sys.stdin.readline()
+        if(str == 'ENTER\n'):
+            answer += len(gomgom)
+            gomgom.clear()
+        else:
+            gomgom.add(str)
+    print(answer + len(gomgom))
+
+problem_25192()
